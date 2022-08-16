d1635ccdba1d64fe69230ab64cae783368c281a3
shkhan922 <shkhan922@gmail.com>
2022-08-17 02:52:10 +0530

    test card added

--- pages/diognostic-center/index.jsx
+++ pages/diognostic-center/index.jsx
@@ -1,9 +1,58 @@
 import React from 'react'
+import Hero from '~/components/ReusableComponent/Hero'
+import Footer from '~/components/ReusableComponent/Footer'
+import Header from '~/components/ReusableComponent/Header'
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button'
 
-const index = () => {
+const Index = () => {
   return (
-    <h1>diognostic center</h1>
+    <>
+    <Header/>
+    <Hero bg='item-bg-3' title='Diagnostic Centers'/>
+    
+    
+    <div className='container' style={{ paddingTop:'3rem', paddingBottom:'3rem'}}>
+      <div className='row' style={{ display:'flex', justifyContent:'space-evenly', flexDirection:'row'}}>
+         
+      <Card style={{ width: '18rem', paddingRight: '2rem' }}>
+      <Card.Body>
+        <Card.Title>Test Name</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">Price </Card.Subtitle>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Pay Now</Button>
+        
+        
+        
+      </Card.Body>
+      </Card>
+         
+      
+        
+    <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>Test Name</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">Price</Card.Subtitle>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        
+        <Button variant="primary">Pay Now</Button>
+      </Card.Body>
+    </Card>
+        
+    
+      </div>
+      </div>
+    
+    <Footer/>
+    
+    </>
   )
 }
 
-export default index
+export default Index
