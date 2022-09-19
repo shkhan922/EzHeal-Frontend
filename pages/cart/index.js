881f13c488387dd6322003bea76a7aa6dd9c8da7
shkhan922 <shkhan922@gmail.com>
2022-09-19 20:37:40 +0530

    razorpay added

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -188,7 +188,7 @@ const Index = () => {
     console.log(data);
     var options = {
       key: process.env.RAZORPAY_KEY, // Enter the Key ID generated from the Dashboard
-      name: "MOMEKZ PVT LTD",
+      name: "Aikenist Pvt Ltd",
       currency: data.currency,
       amount: data.amount,
       order_id: data.id,
@@ -232,7 +232,7 @@ const Index = () => {
         // }).catch(e => console.log(e))
       },
       prefill: {
-        name: 'faijan',
+        name: 'Sahdab',
         email: 'abx@email.com',
         contact: 89893827382,
       },
@@ -696,7 +696,7 @@ const Index = () => {
                         {
                           selectedDioCenter[0].attributes.slot.map((data) => {
                             return (
-                              <div>
+                              <div key={data.id}>
                                 <input type="radio" className='me-2' name='slot' value={data.from + " - " + data.to} onChange={(e) => setSelectedSlot(e.target.value)} />
                                 <span>{data.from} - {data.to}</span>
                               </div>)
